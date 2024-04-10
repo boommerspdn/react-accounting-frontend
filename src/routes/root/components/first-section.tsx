b65af576a599661cc43fd16f952ef816fcf4715b
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2024-04-10 09:41:06 +0700

    add carousel

--- src/routes/root/components/first-section.tsx
+++ src/routes/root/components/first-section.tsx
@@ -1,5 +1,6 @@
 import { ServiceCard } from "@/types";
 import Service from "./service";
+import AdvertistCarousel from "./advertise-carousel";
 
 interface FirstSection {
   title: string;
@@ -10,6 +11,9 @@ interface FirstSection {
 const FirstSection = ({ title, description, services }: FirstSection) => {
   return (
     <section className="container my-16 space-y-8">
+      <div className="h-[300px] w-full bg-slate-200">
+        <AdvertistCarousel />
+      </div>
       <h1 className="text-3xl md:text-5xl">{title}</h1>
       <p className="text-xl">{description}</p>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
